940a6b541ebd486436568d6863b5ef1814df6c8e
Evan Krueger <evank0301@gmail.com>
2025-04-19 16:00:36 -0500

    add lsp

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.keymap.set('i', 'jk', '<Esc>')
+
 require("config.lazy")
 require("config.telescope")
 require("config.nvimtreesitter")
