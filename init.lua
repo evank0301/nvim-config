de024a706779ce248e07e95c7c8e60e11b5936da
Evan Krueger <evank0301@gmail.com>
2025-08-24 20:41:03 -0500

    updates

--- init.lua
+++ init.lua
@@ -6,8 +6,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set foldmethod=syntax")
 
 vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('n', 'H', '<C-w>h')
+vim.keymap.set('n', 'L', '<C-w>l')
+vim.keymap.set('n', 'J', '<C-w>j')
+vim.keymap.set('n', 'K', '<C-w>k')
 
 require("config.lazy")
 require("config.telescope")
